20514f5503a61d284ed7422586ef60f466e09661
zhangxuanforfree <11302106@qq.com>
2022-01-25 15:56:43 +0800

    frist upload

---
+++ src/main/java/net/mcreator/slingshotmod/entity/renderer/SlingshotRenderer.java
@@ -0,0 +1,24 @@
+package net.mcreator.slingshotmod.entity.renderer;
+
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.client.renderer.entity.SpriteRenderer;
+import net.minecraft.client.Minecraft;
+
+import net.mcreator.slingshotmod.item.SlingshotItem;
+
+@OnlyIn(Dist.CLIENT)
+public class SlingshotRenderer {
+	public static class ModelRegisterHandler {
+		@SubscribeEvent
+		@OnlyIn(Dist.CLIENT)
+		public void registerModels(ModelRegistryEvent event) {
+			RenderingRegistry.registerEntityRenderingHandler(SlingshotItem.arrow,
+					renderManager -> new SpriteRenderer(renderManager, Minecraft.getInstance().getItemRenderer()));
+		}
+	}
+}
